e572f5cd9c95abaf54bb2d39c2adec90483e126c
Reshma Narute <>
2021-06-04 21:56:26 +0000

    corrected 3rd question

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,7 +2,7 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
-let questions=["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS "];
+let questions=["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswers =["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswers = [];
 
